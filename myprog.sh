164e7cfc4a299dea3e4e3db29d8aacbbc7c9e372
esra-polat <esrapolat@gmail.com>
2020-11-21 01:55:47 +0300

    edited menu

--- myprog.sh
+++ myprog.sh
@@ -1,5 +1,4 @@
-while :
-do
+while :; do
     clear
     echo "1. Create histogram"
     echo "2. Encryption"
@@ -10,32 +9,44 @@ do
     read -p "Please select an option: " choice
     case $choice in
 
-        1)  echo "Please enter a filename: "
-            read arg
-            exec ./myprog1.sh "arg" ;;
+    1)
+        echo "Please enter a filename: "
+        read arg
+        exec ./myprog1.sh "arg"
+        ;;
 
-        2)  echo "Please enter a string and a number: "
-            read arg
-                arr=($arg)
-            exec ./myprog2.sh "${arr[0]}" "${arr[1]}" ;;
+    2)
+        echo "Please enter a string and a number: "
+        read arg
+        arr=($arg)
+        exec ./myprog2.sh "${arr[0]}" "${arr[1]}"
+        ;;
 
-        3)  echo "If you want, please enter pathname or continue without argument: "
-            read arg
-            exec ./myprog3.sh "arg" ;;
+    3)
+        echo "If you want, please enter pathname or continue without argument: "
+        read arg
+        exec ./myprog3.sh "arg"
+        ;;
 
-        4)  echo "Please enter a filename : "
-            read arg
-            exec ./myprog4.sh "arg" ;;
+    4)
+        echo "Please enter a filename : "
+        read arg
+        exec ./myprog4.sh "arg"
+        ;;
 
-        5)  echo "If you want, please enter a wildcard with -R or continue without wildcard: "
-            read arg
-                arr=($arg)
-            exec ./myprog2.sh "${arr[0]}" "${arr[1]}" ;;
+    5)
+        echo "If you want, please enter a wildcard with -R or continue without wildcard: "
+        read arg
+        arr=($arg)
+        exec ./myprog2.sh "${arr[0]}" "${arr[1]}"
+        ;;
 
-        6)  exit 0 ;;
+    6) exit 0 ;;
 
-        *) echo "Opps! Please select choice 1, 2, 3, 4, 5 or 6"
+    *)
+        echo "Opps! Please select choice 1, 2, 3, 4, 5 or 6"
         echo "Press a key: "
-        read;;
+        read
+        ;;
     esac
 done
